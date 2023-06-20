14fbed471b3085a0293c8491ec8a5489334b8423
Muhammad Idrees <121349496+idreesishaq@users.noreply.github.com>
2023-06-20 05:52:18 +0500

    app.js file of task chapter_14-16, added.

---
+++ assignments/chapter_14-16_array/app.js
@@ -0,0 +1,181 @@
+/*-------------Question No 1--------------*/
+
+//var student = [];
+
+
+
+
+
+/*-------------Question No 2--------------*/
+
+//var students = {}
+//alert(students)
+//console.log(students);
+
+
+
+
+/*-------------Question No 3--------------*/
+
+// var Electronics = ["computer", "mobile", "laptop", "watch"];
+// document.write(Electronics + "<br>");
+
+
+
+
+
+/*-------------Question No 4--------------*/
+
+// var Price = [12000, 6300, 4250, 2190];
+// document.write(Price);
+
+
+
+/*-------------Question No 5--------------*/
+
+// var bol = [true, false];
+//  alert(bol[0]);
+
+
+// if(bol[1]){
+//     alert("Yes! You are in");
+// }
+// else{
+//     alert("I'm afraid. You can't in");
+// }
+
+
+
+
+
+/*-------------Question No 6--------------*/
+
+// var msc = ["Jacob", 39, true];
+// console.log(msc[0]);
+// console.log(msc[1]);
+// console.log(msc[2]);
+
+
+
+/*-------------Question No 7--------------*/
+//var Qualification = ["SSC", "HSC", "BSC", 'BS', 'BCOM', 'MS', 'M.Phil', 'PhD'];
+
+
+
+
+
+
+
+/*-------------Question No 8--------------*/
+
+// var studentName = ["Michael", "John", "Tony"];
+// var studentScore = [320, 230, 480];
+
+// document.write("Score of " + studentName[0] + " is " + studentScore[0] + ". Percentage: " + studentScore[0]/500*100+"% <br>");
+// document.write("Score of " + studentName[1] + " is " + studentScore[1] + ". Percentage: " + studentScore[1]/500*100+"% <br>");
+// document.write("Score of " + studentName[2] + " is " + studentScore[2] + ". Percentage: " + studentScore[2]/500*100+"% <br>");
+
+
+
+
+
+
+
+/*-------------Question No 9--------------*/
+// var colors = ["IndianRed", "LightCoral", "Salmon", "Crimson", "Red", "Pink", "Orange", "OrageRed","Gold", "Yellow", "Khaki", "Lavender", "Thistle", "Plum", "Orchid", "Fuchsia", "Magenta", "Purple", "Indigo", "SlateBlue", "Green", "Cyan", "Teal", "BodgerBlue", "Blue", "Navy", "Wheat", "Tan", "Peru", "Sienna", "Brown", "Maroon", "Azure", "Beige", "White", "Gray", "Black"]
+// document.write(colors);
+var colours = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+// document.write(colours + "<br><br>");
+
+/*-------------Question No 9 (a) --------------*/
+
+// var colourAd = prompt("Enter the colour name you want to add");
+
+
+
+/* shift remove an element from the end of the array
+and unshift adds one or more elements to the beginning of the array */
+
+// colours.unshift(colourAd);
+// document.write(colours);
+
+// /* We can add an element against any perticular index of an array */
+// colours[10] = "brick"
+// document.write(colours);
+
+
+/*-------------Question No 9 (b) --------------*/
+
+// /*push adds the element in the end of the array */
+// // colours.push(colorAd);
+// // document.write(colours);
+// var colourAddEnd = prompt("Enter the colour name that you want to add at the end of array colours");
+// colours.push(colourAddEnd);
+// document.write(colours);
+
+
+/*-------------Question No 9 (c) --------------*/
+// colours.unshift("Purple", "Pink");
+// document.write(colours);
+// document.write("<br");
+
+/*-------------Question No 9 (d) --------------*/
+
+// colours.shift();
+// document.write(colours);
+// // document.write("<br>");
+
+
+/*-------------Question No 9 (e) --------------*/
+// colours.pop();
+// document.write(colours);
+
+
+/*-------------Question No 9 (f) --------------*/
+// var index = prompt("Enter the index");
+
+
+// var newColour = prompt("Enter the Colour name");
+
+// colours.splice(index, 0, newColour);
+// document.write(colours);
+
+
+/*-------------Question No 9 (g) --------------*/
+// var elDel = prompt("Enter an index number from where to start delete");
+// var elDel2 = prompt("How many elements be deleted");
+// document.write(colours + "<br>");
+// colours.splice(elDel, elDel2);
+// document.write(colours);
+
+
+/*-------------Question No 10 --------------*/
+// var studentScores = [320, 230, 480, 120]
+// document.write("Scores of Students: " + studentScores + "<br>");
+// document.write("Ordered Scores of Students: " + studentScores.sort());
+
+
+
+/*-------------Question No 11--------------*/
+// var cityNames = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// document.write("Cities list: <br>" + cityNames + "<br><br>");
+// document.write("Selected cities list: <br>" + cityNames.slice(2, 4));
+
+
+
+
+/*-------------Question No 12--------------*/
+// var arr = ["This", "is", "my", "cat"];
+// document.write("Array <br>" + arr +"<br><br>");
+// document.write("String: <br>" + arr.join(" "));
+
+
+
+
+/*-------------Question No 13--------------*/
+
+
+/*-------------Question No 14--------------*/
+
+/*-------------Question No 15--------------*/
+
