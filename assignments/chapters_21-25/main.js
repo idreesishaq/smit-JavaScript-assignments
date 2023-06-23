a88d7b2d034ddd904232cf8c876e0fa1d80fac4c
Muhammad Idrees <121349496+idreesishaq@users.noreply.github.com>
2023-06-23 21:17:14 +0500

    task chapter21-25's pdf and main.js files added

---
+++ assignments/chapters_21-25/main.js
@@ -0,0 +1,72 @@
+/*--------------------CHAPTER 21-25--------------------*/
+
+
+
+/*-------------------- Question No 1 --------------------*/
+// var firstName = prompt("Enter first name","ali");
+// var lastName = prompt("Enter last name","faraz");
+// var fullName = firstName +" " +lastName;
+// alert("Assalam-o-Alaikum! Mr. " + fullName);
+
+
+
+/*-------------------- Question No 2 --------------------*/
+// var fvtMobile = prompt("Please enter your favorite Mobile Model");
+// var charLen = fvtMobile.length;
+// document.write("<br>My favorite phone is: "+ fvtMobile);
+// document.write("<br>Length of string: "+ charLen);
+
+/*-------------------- Question No 3 --------------------*/
+// var nationality = "Pakistani";
+// var index = nationality.indexOf("n");
+// document.write("<br>String: "+ nationality);
+// document.write("<br>Index of 'n': " + index);
+
+/*-------------------- Question No 4 --------------------*/
+// var string2 = "Hello World";
+// var lastIndex = string2.lastIndexOf("l");
+// document.write("String: "+ string2);
+// document.write("<br>Last index of 'l': "+ lastIndex);
+
+
+/*-------------------- Question No 5 --------------------*/
+// var string3 = "Pakistani";
+// var character = string3.charAt(3);
+// document.write("String: " + string3);
+// document.write("<br>Character at index 3: " + character);
+
+
+/*-------------------- Question No 6 --------------------*/
+// var firstName2 = prompt("Enter Your First Name");
+// var lastName2 = prompt("Enter Your Last Name");
+// var fullName2 = firstName2.concat(" "+lastName2);
+// alert("Wellcome Mr. "+fullName2);
+
+
+/*-------------------- Question No 7 --------------------*/
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam");
+// document.write("City: " + city);
+// document.write("<br>After replacement: " + newCity);
+
+/*Alternative Method*/
+// var city2 = "Faisalabad";
+// for(let i=0; i < city2.length; i++){
+//     if(city2.slice(i,i+6)==="Faisal"){
+//         var newCity2 = city2.slice(0,i) + "Abbot" + city2.slice(i+6);
+//     }
+// }
+// alert(newCity2);
+
+
+/*-------------------- Question No 8 --------------------*/
+var message = "Ali and Sami are best friends. They play cricket and football together. Their ages are 14 and 15 respectively.";
+document.write(message);
+
+var newMessage = message.replace("and", "&");
+document.write("<br><br>"+ newMessage);
+
+/*-------------------- Question No 6 --------------------*/
+
+/*-------------------- Question No 6 --------------------*/
+
