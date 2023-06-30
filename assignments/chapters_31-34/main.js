5a7b098c6af27237c88f3632f9c3e238b87435d5
Muhammad Idrees <121349496+idreesishaq@users.noreply.github.com>
2023-06-30 15:15:35 +0500

    Update main.js of JS task chapter31-34_Date

--- assignments/chapters_31-34/main.js
+++ assignments/chapters_31-34/main.js
@@ -39,8 +39,8 @@ document.write("<br> The Date: " + theDate);
 var halfMonthMsg = "First fifteen days of the month";
 var halfMonthMsg2 = "Last days of the month";
 
-if(theDate < 16){
-    document.write("<br>"+ halfMonthMsg);
+if (theDate < 16) {
+    document.write("<br>" + halfMonthMsg);
 } else {
     document.write("<br>" + halfMonthMsg2);
 }
@@ -51,16 +51,16 @@ if(theDate < 16){
 document.write("<br> Current Date: " + today);
 
 var theTime = today.getTime();
-document.write("<br> The Time: " + theTime );
+document.write("<br> The Time: " + theTime);
 document.write("<br> Elapsed milliseconds since January 1, 1970: " + theTime);
-document.write("<br> Elapsed minutes since January 1, 1970: " + theTime/1000/60);
+document.write("<br> Elapsed minutes since January 1, 1970: " + theTime / 1000 / 60);
 
 
 
 /*------------------------------ QUESTION NO. 7 ------------------------------*/
 var currHour = today.getHours();
 document.write("<br>" + currHour)
-if(currHour < 12){
+if (currHour < 12) {
     document.write("<br> It's AM");
 } else {
     document.write("<br> It's PM");
@@ -70,14 +70,23 @@ if(currHour < 12){
 
 /*------------------------------ QUESTION NO. 8 ------------------------------*/
 var lastDayOf2020 = new Date("Dec 31, 2020");
-document.write("<br> lastDay of 2020: "+ lastDayOf2020);
+document.write("<br> lastDay of 2020: " + lastDayOf2020);
 
 
 
 /*------------------------------ QUESTION NO. 9 ------------------------------*/
+var firstDayRamadan = new Date("Mar 23, 2023");
+var dayDiff = today - firstDayRamadan;
+var dayPast = Math.floor(dayDiff / 1000 / 60 / 60 / 24);
+document.write("<br>" + dayPast + " days have passed since 1st Ramadan, 2023");
 
 
 /*------------------------------ QUESTION NO. 10 ------------------------------*/
+var timePassReference = new Date("Dec 05, 2015 22:50:16 GMT+0500 (PKT)").getTime() - new Date("Jan 1, 2015").getTime();
+var secPass = timePassReference / 1000;
+var daysPass = Math.floor(timePassReference / 1000 / 60 / 60 / 24);
+document.write("<br> On reference date Sat Dec 05 2015 22:50:16 GMT+0500 (PKT), <br> "
+    + secPass + " Seconds and " + daysPass + " Days had been passed since the beginning of 2015");
 
 
 /*------------------------------ QUESTION NO. 11 ------------------------------*/
