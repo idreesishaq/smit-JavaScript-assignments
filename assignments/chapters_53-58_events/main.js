ae688ccaa7b75370d5413c160f6211a85f871606
Muhammad Idrees <121349496+idreesishaq@users.noreply.github.com>
2023-08-23 23:13:51 +0500

    JS task chapters53-58 main.js created.

---
+++ assignments/chapters_53-58_events/main.js
@@ -0,0 +1,15 @@
+var img = document.querySelectorAll(".img");
+for(let i=0; i<img.length; i++){
+    img[i].setAttribute("onclick", "modal()");
+}
+function modal(){
+    document.getElementById("modal-container").style.display="block";
+    let imgSrc = event.target.src;
+    document.getElementById("modal-img").src=`${imgSrc}`;
+    document.getElementById("body").style.backgroundColor="gray";
+}
+
+function closeImg(){
+    document.getElementById("modal-container").style.display="none";
+    document.getElementById("body").style.backgroundColor="white";
+}
