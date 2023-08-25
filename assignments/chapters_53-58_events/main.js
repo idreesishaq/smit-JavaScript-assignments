cc3da267747bb1531865d40666f3916ae6947301
Muhammad Idrees <121349496+idreesishaq@users.noreply.github.com>
2023-08-25 20:46:03 +0500

    JS task chapters53-58 Q2 solved & added in main.js

--- assignments/chapters_53-58_events/main.js
+++ assignments/chapters_53-58_events/main.js
@@ -1,15 +1,35 @@
 var img = document.querySelectorAll(".img");
-for(let i=0; i<img.length; i++){
+for (let i = 0; i < img.length; i++) {
     img[i].setAttribute("onclick", "modal()");
 }
-function modal(){
-    document.getElementById("modal-container").style.display="block";
+function modal() {
+    document.getElementById("modal-container").style.display = "block";
     let imgSrc = event.target.src;
-    document.getElementById("modal-img").src=`${imgSrc}`;
-    document.getElementById("body").style.backgroundColor="gray";
+    document.getElementById("modal-img").src = `${imgSrc}`;
+    document.getElementById("body").style.backgroundColor = "gray";
 }
 
-function closeImg(){
-    document.getElementById("modal-container").style.display="none";
-    document.getElementById("body").style.backgroundColor="white";
+function closeImg() {
+    document.getElementById("modal-container").style.display = "none";
+    document.getElementById("body").style.backgroundColor = "white";
+}
+
+
+
+/*------------- QUESTION NO 2---------------*/
+
+function zoomOut() {
+    var element = document.getElementById("zoom");
+    var FontSizeString = window.getComputedStyle(element).fontSize;
+    var numericFontSize = parseInt(FontSizeString);
+    numericFontSize -= 10;
+    element.style.fontSize = `${numericFontSize}px`;
+}
+
+function zoomIn() {
+    let element = document.getElementById("zoom");
+    let FontSizeString = window.getComputedStyle(element).fontSize;
+    let numericFontSize = parseInt(FontSizeString);
+    numericFontSize += 10;
+    element.style.fontSize = `${numericFontSize}px`;
 }
